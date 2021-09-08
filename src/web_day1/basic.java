80cc97842798143105fdabffbe29ab4748bfc1aa
JungWooLee <wjddn3711@naver.com>
2021-09-08 21:51:52 +0900

    web day1

---
+++ src/web_day1/basic.java
@@ -0,0 +1,53 @@
+package web_day1;
+
+public class basic {
+	public static void main(String[] args) {
+//		int a;
+//		String b;
+		
+		// 변수 선언
+//		a = 1;
+//		b = "Hello World!";
+		
+//		int a = 1;
+//		String b = "Jung Jung";
+//		
+//		System.out.println(a);
+//		System.out.println(b);
+		
+		// 연산자 
+//		int a = 10;
+//		int b = 5;
+//		System.out.println(a%b); 0
+//		System.out.println(b%a); 5
+		
+//		int i = 0;
+//		i++; // i=i+1, i+=1
+//		
+//		System.out.println(i);
+//		System.out.println(i++); //후위연산자는 대입하고 더해준다 
+//		System.out.println(++i); //전위 연산자는 더해주고 대입한
+		
+//		String a = "hello";
+//		String b = "java";
+//		String c = "hello";
+//		System.out.println(a.equals(b)); //false 
+//		System.out.println(a.equals(c)); //true
+//		
+//		System.out.println(a==b); //false
+//		System.out.println(a==c); //true
+		
+//		String d = "hello";
+//		String e = new String("hello");
+//		System.out.println(d==e); //false
+//		System.out.println(d.equals(e)); //true
+		
+		
+		// indexOf
+		String a = "Hello Java";
+		System.out.println(a.indexOf("Java"));
+		
+		
+		
+	}
+}
